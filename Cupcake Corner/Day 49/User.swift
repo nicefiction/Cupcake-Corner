555317229a4862732313c115efed8894d27f71e4
Olivier Van hamme <nicefiction@gmail.com>
2021-07-24 10:58:28 +0200

    Completes Day 49.1 : Adds Codable conformance for @Published properties .

---
+++ Cupcake Corner/Day 49/User.swift
@@ -0,0 +1,49 @@
+// User.swift
+
+import Foundation
+
+
+final class User: ObservableObject,
+            Codable {
+   
+   // MARK: - PROPERTY WRAPPERS
+   
+   @Published var name: String
+   
+   
+   
+   // MARK: - NESTED TYPES
+   
+   enum CodingKeys: CodingKey {
+      
+      case name
+   }
+   
+   
+   
+   // MARK: - INITIALIZER METHODS
+   
+   init(from decoder: Decoder)
+   throws {
+      
+      // let jsonDecoder = JSONDecoder()
+      
+      let userDecodingContainer = try decoder.container(keyedBy: CodingKeys.self)
+      
+      self.name = try userDecodingContainer.decode(String.self,
+                                           forKey: CodingKeys.name)
+   }
+   
+   
+   
+   // MARK: - METHODS
+   
+   func encode(to encoder: Encoder)
+   throws {
+      
+      var userEncodingContainer = encoder.container(keyedBy: CodingKeys.self)
+      
+      try userEncodingContainer.encode(name,
+                                       forKey: CodingKeys.name)
+   }
+}
